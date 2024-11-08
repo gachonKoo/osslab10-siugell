56838818cfe94920fdb4324c1ab6196b97f18925
siugell <168634593+siugell@users.noreply.github.com>
2024-11-09 03:00:28 +0900

    Update divisor.py

--- divisor.py
+++ divisor.py
@@ -1,9 +1,10 @@
 import sys
 
-number = int(sys.argv[1])
+number = int(sys.argv[1])  # 명령줄에서 입력받은 값을 정수로 변환
 
+# 1부터 입력받은 number까지 반복
 for i in range(1, number + 1):
-    if number % i == 0:
-        print(i, end=" ")
+    if number % i == 0:  # i가 number의 약수인지 확인
+        print(i, end=" ")  # 약수를 한 줄에 출력
 
-print()
+print()  # 줄바꿈
